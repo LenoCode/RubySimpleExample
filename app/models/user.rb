e85603553acff3d5deffddfa4086cb9509dea4ae
leno <leno.application@gmail.com>
2024-01-07 17:17:21 +0100

    First model test for user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
   has_many :api_tokens
 
+  validate :name_presence
+
+
+  private
+
+  def name_presence
+    errors.add(":name","Cant be number, only letters") if name.scan(/\D/).empty?
+  end
 
 end
