c2d5aefcaf93b086542fdde7427d33eea212fa0e
leno <leno.application@gmail.com>
2024-01-08 00:46:52 +0100

    Added test for Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,13 @@
 class Book < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
+  validate :validate_book
+
+  private
+
+  def validate_book
+    user = User.find_by(id: author_id)
+    roles = XrefUserRole.joins(:user).joins(:user_role).where("users.name = ?", user.name).where("user_roles.name= ?","Author")
+    errors.add("roles","This user does not have Author role") if roles.empty?
+  end
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -5,12 +5,30 @@ RSpec.describe Book, type: :model do
 
   describe "Simple validation" do
 
+    context "Book creation, invalid examples" do
+
+      it "Create book with user who is not author" do
+        ruby_user = User.create name: "Ruby"
+        expect(ruby_user).to be_valid
+
+        ruby_book = Book.create title:"Ruby life", copies: 232, author:ruby_user
+        expect(ruby_book.errors["roles"]).to include("This user does not have Author role")
+
+      end
+
+    end
 
     context "Book creation, valid examples" do
 
       it "Create book referenced to dummy user" do
         ruby_user = User.create name: "Ruby"
         expect(ruby_user).to be_valid
+        ruby_user.save!
+
+        authorRole = UserRole.create! name:"Author"
+        authorRole.save!
+
+        xrefUserRole = XrefUserRole.create! user: ruby_user, user_role: authorRole
 
         ruby_book = Book.create title:"Ruby life", copies: 232, author:ruby_user
         expect(ruby_book).to be_valid
