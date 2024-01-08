7321c9696fff59da2893387e5bfcedcf005e5851
leno <leno.application@gmail.com>
2024-01-08 01:45:44 +0100

    Added catalog controller

---
+++ app/controllers/api/v1/catalog_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class Api::V1::CatalogController < Api::V1::AuthenticatedController
+
+
+
+end
---
+++ spec/requests/catalog_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "Catalogs", type: :request do
+
+
+
+  describe "" do
+
+  end
+
+end
