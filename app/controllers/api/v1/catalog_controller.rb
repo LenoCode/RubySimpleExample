0cc39d868f3b4cdc90cf1b53b3aa94e1f44d90ea
leno <leno.application@gmail.com>
2024-01-08 03:04:55 +0100

    Finished end point for search_books and test is passing

--- app/controllers/api/v1/catalog_controller.rb
+++ app/controllers/api/v1/catalog_controller.rb
@@ -5,5 +5,17 @@ class Api::V1::CatalogController < Api::V1::AuthenticatedController
 
 
 
+  def search_books
+    author_param= params[:author]
+    title_params = params[:title]
+
+
+    if title_params.nil?
+      render json: {result: JSON.parse(Book.all.to_json(only: [:id,:title,:author,:copies]))}, status: ok
+    else
+      render json: {result: JSON.parse(Book.filter_by_title_and_author(title_params,author_param).to_json(only: [:id,:title, :author, :copies])) }, status: :ok
+    end
+
+  end
 
 end
--- spec/requests/catalog_spec.rb
+++ spec/requests/catalog_spec.rb
@@ -10,6 +10,13 @@ RSpec.describe "Catalogs", type: :request do
     Book.create! title:"Book1-life", copies: 120, author: user
     Book.create! title:"Book2-social", copies: 120, author: user
     Book.create! title:"Book3-code", copies: 120, author: user
+
+
+    apitoken = ApiToken.create!(user:user)
+    apitoken.save
+
+    @token = apitoken.token
+
   end
 
 
@@ -20,19 +27,18 @@ RSpec.describe "Catalogs", type: :request do
       it 'Test simple search test wih both parameter correct (author and title)' do
 
         params = {
-          "title":"Book",
+          "title":"Book1",
           "author":"Ruby"
         }
-
-        get api_v1_catalog_search_path params: params
+        get api_v1_catalog_search_path, params: params, headers: {'Authorization' => "Bearer #{@token}"}
 
         expect(response).to have_http_status(:ok)
 
         result = JSON.parse(response.body)
 
+        expect(result["result"].length).to eql(1)
 
-        expect(result["list"].length).to eql(0)
-
+        expect(result["result"][0]["title"]).to eql("Book1-life")
 
       end
 
@@ -44,6 +50,8 @@ RSpec.describe "Catalogs", type: :request do
 
   after(:all) do
     Book.delete_all
+    ApiToken.delete_all
+    User.delete_all
   end
 
 end
